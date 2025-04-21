23267738941ee3a4d7ee7ac677cfad1e9c2166f9
sihwakr <100psk@naver.com>
2025-04-21 14:06:14 +0900

    first commt

---
+++ src/main/java/com/example/puzzlelab/PuzzlelabApplication.java
@@ -0,0 +1,13 @@
+package com.example.puzzlelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzlelabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzlelabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/puzzlelab/PuzzlelabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.puzzlelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzlelabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
